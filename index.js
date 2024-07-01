0357ecedd7faf67b0570beed40fbe34f1d54bb70
Lenrie13 <lenrie.ogutu13@gmail.com>
2024-07-01 22:52:12 +0300

    Work out arrow functions lab

---
+++ index.js
@@ -0,0 +1,46 @@
+// function writePoetry(poem) {
+
+// }
+// const printBook = book => {
+//     // console.log(book); OR
+//     return book;
+// }
+
+// or 
+// // const cookDinner = (Food) => console.log(Food);
+//  or
+// //  const cookDinner = food => food;
+// //  Does not need the return keyword
+// const cookDinner = (food, soup) => [food, soup]; 
+
+let a = 2000;
+let b = 100;
+
+const divide = (a, b) =>  {
+    return a / b;
+};
+
+let x = 2;
+const square = x => x ** 2;
+    square(x);
+
+// replaced a and b with c and d since they cannot be redeclared
+let c = 3;
+let d = 4;
+
+const add = (c, d) => c + d;
+    add(c, d);
+
+    
+
+
+
+
+    
+
+
+// function divide (a, b) {
+//     return a/b
+// }
+
+// console.log(divide(2000, 100));
